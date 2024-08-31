a2ccb70229821efbc340f75ad3053021d52640c1
Valyushka2023 <vvpto@ukr.net>
2024-09-01 00:35:45 +0300

    Початковий коміт гілки hw2-mongodb

--- src/constants/contacts.js
+++
@@ -1,8 +0,0 @@
-import * as path from "node:path";
-
-export const PATH_DB = path.join("src", "db", "db.json");
-
-
-
-
-
---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import pino from "pino-http";
+import { env } from "./utils/env.js";
+import contactsRouter from "./routers/contacts.js";
+
+
+
+export const setupServer = () => {
+  const app = express();
+
+  const logger = pino({
+    transport: {
+      target: "pino-pretty",
+    },
+  });
+
+  app.use(logger);
+  app.use(cors());
+  app.use(express.json());
+
+
+  app.use("/contacts", contactsRouter);
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: `${req.url} not found`,
+    });
+  });
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message,
+    });
+  });
+
+  const port = Number(env("PORT", 3000));
+
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+};
